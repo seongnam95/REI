cba640512fc042e8117aa078587fc4e6784fa5f3
seongnam95 <jsn_@naver.com>
2022-02-24 03:19:33 +0900

    Changes

--- test/selenium_test/test2.py
+++ test/selenium_test/test2.py
@@ -4,7 +4,7 @@ from urllib.parse import urljoin
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
 
 
@@ -15,24 +15,24 @@ import time
 
 options = webdriver.ChromeOptions()
 
-options.add_argument('headless')
-options.add_argument("no-sandbox")
-options.add_argument('window-size=1920x1080')
+# options.add_argument('headless')    # 크롬 화면 숨기기
+options.add_argument("no-sandbox")  #
+# options.add_argument('window-size=1920x1080') # 해상도 설정
 options.add_argument("disable-gpu")   # 가속 사용 x
 options.add_argument("lang=ko_KR")    # 가짜 플러그인 탑재
-options.add_argument('user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')  # user-agent 이름 설정
+# options.add_argument('user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')  # user-agent 이름 설정
 
-driver = webdriver.Chrome(r'C:/chromedriver/chromedriver.exe') # , chrome_options=options)
+old_address = '면목동 90-27'
+new_address = '봉우재로154'
+ho = ['702']
+
+driver = webdriver.Chrome('./chromedriver_98.0.4758.102.exe', chrome_options=options)
 
 driver.get("http://cloud.eais.go.kr/moct/awp/abb01/AWPABB01F01?returnUrl=%2F")
 driver.implicitly_wait(2)
 try:
     driver.find_element(By.CSS_SELECTOR,
                         'body > div.swal-overlay.swal-overlay--show-modal > div > div.swal-footer > div:nth-child(2) > button').click()
-except:
-    pass
-
-try:
     driver.find_element(By.CSS_SELECTOR, '#noticeModal > div > div.noticeClose > ul > li:nth-child(2) > button').click()
 except:
     pass
@@ -46,21 +46,90 @@ driver.find_element(By.CSS_SELECTOR, '#container > div.content.pb80 > div > div 
 
 try:
     driver.find_element(By.CSS_SELECTOR, '#container > div.content.pb80 > div > div > div.fr > div.btns > button.btnDaum.btnSolid.btnLarge').click()
+    time.sleep(1)
     driver.find_element(By.CSS_SELECTOR, 'body > div.swal-overlay.swal-overlay--show-modal > div > div.swal-footer > div:nth-child(2) > button').click()
 except:
     pass
+time.sleep(2)
+driver.execute_script("window.scrollTo(0, 700)")
+print('로그인')
 
-driver.find_element(By.CSS_SELECTOR, '#section1 > div > div.mainInner > div.registerUi > div.bldreDiv.bldre1 > a').click()
-driver.find_element(By.CSS_SELECTOR, '#container > div.content.clearFix > div > div.floatWarp.mt30.clearFix > div.contLeft > div.srchArchitecture > div.searchBuildingWarp > div.AddrSearch > button.btnAddrSrch').click()
-driver.find_element(By.CSS_SELECTOR, '#keyword').send_keys('중랑구 봉우재로154')
-driver.find_element(By.CSS_SELECTOR, '#container > div.content.clearFix > div > div.floatWarp.mt30.clearFix > div.contLeft > div.srchArchitecture > div.popAddrSearch > div > div.formIn > button').click()
 
-listt = driver.find_element(By.XPATH, '//*[@id="container"]/div[2]/div/div[2]/div[1]/div[1]/div[3]/div/div[3]/ul')
-for n, i in enumerate(listt.find_elements(By.TAG_NAME, 'li')):
-    print(i.accessible_name)
-    print(n, i.text)
+def find_ho():
+    driver.find_element(By.CLASS_NAME, 'bldreDiv.bldre1').click()
+    driver.find_element(By.CLASS_NAME, 'btnAddrSrch').click()
+    driver.find_element(By.CSS_SELECTOR, '#keyword').send_keys('봉우재로154')
+    driver.find_element(By.CLASS_NAME, 'btnNext.btnSolid.btnNormal.btn_dark').click()
 
+    listt = driver.find_element(By.CLASS_NAME, 'addrList').find_element(By.TAG_NAME, 'ul')
+    time.sleep(2)
+    for n, i in enumerate(listt.find_elements(By.TAG_NAME, 'li')):
+        adr = i.text.split('\n선택')[0]
+        if old_address in adr:
+            i.find_element(By.TAG_NAME, 'button').click()
+            break
+    time.sleep(2)
+    driver.find_element(By.XPATH, '//*[@id="container"]/div[2]/div/div[2]/div[1]/div[3]/ul/li[5]/a').click()
+    time.sleep(1)
+    lst = driver.find_element(By.XPATH, '//*[@id="container"]/div[2]/div/div[2]/div[1]/div[3]/div/div[5]/table/tbody/div/div/div[1]/div[2]/div[3]/div[2]/div/div')
+
+    # 전유부 클릭
+    ho_count, breaker = 0, False
+    for a, b in enumerate(lst.find_elements(By.TAG_NAME, 'div')):
+        if b.get_attribute("row-id"):
+            for n, i in enumerate(b.find_elements(By.TAG_NAME, 'div')):
+                if i.get_attribute('col-id') == 'hoNm':
+                    if i.get_attribute("innerText") in ho:
+                        ho_count += 1
+                        b.find_element(By.XPATH, 'div[1]/div/div/div/div[2]').find_element(By.TAG_NAME, 'input').click()
+                    if ho_count == len(ho):
+                        breaker = True
+                        break
+            if breaker: break
+    driver.find_element(By.CLASS_NAME, 'btnAddCart').click()
+    time.sleep(1)
+    driver.find_element(By.CLASS_NAME, 'btnSubmit.mt10').click()
+    driver.find_element(By.XPATH, '//*[@id="container"]/div[2]/div/div[2]/ul/li/ul/li[2]').click()
+    driver.find_element(By.XPATH, '//*[@id="container"]/div[2]/div/div[5]/button[2]').click()   # 민원 신청
+
+
+find_ho()
+# driver.find_element(By.CLASS_NAME, 'bldreDiv.bldre3').click() # 발급현황 ㅋㄹ릭
 time.sleep(3)
+driver.refresh()
+time.sleep(1)
+lst = driver.find_element(By.XPATH, '//*[@id="container"]/div[2]/div/div[4]/table/tbody')
+
+breaker = False
+for i in lst.find_elements(By.TAG_NAME, 'tr'):
+    print('1번 for', i)
+    content = i.get_attribute("innerText")
+    if old_address in content:
+        if '열람' in content:
+            i.find_element(By.XPATH, 'td[5]/a').click()
+            breaker = True
+
+            # for t in i.find_elements(By.TAG_NAME, 'td'):
+            #     print('2번 for', t)
+            #     try:
+            #         t.find_element(By.TAG_NAME, 'a').click()
+            #         breaker = True
+            #         break
+            #     except: pass
+
+    if breaker: break
+if breaker:
+    time.sleep(3)
+    driver.switch_to.window(driver.window_handles[1])
+    driver.find_element(By.CLASS_NAME, 'report_menu_button.report_menu_print_button.report_menu_print_button_svg').click()
+    select = Select(driver.find_element(By.XPATH, '/html/body/div/div[2]/div/div[1]/select'))
+    select.select_by_visible_text('PDF')
+    print('여까지')
+    driver.find_element(By.CLASS_NAME, 'report_view_button').click()
+time.sleep(3)
+
+driver.quit()
+
 # //*[@id="container"]/div[2]/div/div[2]/div[1]/div[1]/div[3]/div/div[3]/ul/li[1]/text()
 # container > div.content.pb80 > div > div > div.fl > div.loginForm > button
 # driver.get("http://www.gov.kr/mw/AA020InfoCappView.do?CappBizCD=15000000098")
@@ -86,5 +155,3 @@ time.sleep(3)
 # for i in addreess_list:
 #
 #
-driver.get_screenshot_as_file('a.png')
-driver.quit()
