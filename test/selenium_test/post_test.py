2805c6bb40dc63f964b6d6023affd342338091ae
seongnam95 <jsn_@naver.com>
2022-03-19 22:52:36 +0900

    Changes

--- test/selenium_test/post_test.py
+++ test/selenium_test/post_test.py
@@ -1,82 +1,121 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from seleniumrequests import Chrome
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from webdriver_manager.chrome import ChromeDriverManager
-
+import json
 import requests
 import ssl
 import clipboard as clip
 from bs4 import BeautifulSoup as bs
 import time
 
-ssl._create_default_https_context = ssl._create_unverified_context
+# ssl._create_default_https_context = ssl._create_unverified_context
 
 LOGIN_INFO = {
-    'loginId': 'haul1115',
-    'loginPwd': 'ks05090818@'
+    'email': 'haul1115@naver.com',
+    'password': 'ks05090818@'
 }
 
+# create a session
+def CreateSession():
+    chrome_options = webdriver.ChromeOptions()
+    # chrome_options.add_argument('headless')  # 크롬 화면 숨기기
+    chrome_options.add_argument("no-sandbox")  #
+    chrome_options.add_argument('window-size=1920x1080')  # 해상도 설정
+    chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument("disable-gpu")  # 가속 사용 x
+    chrome_options.add_argument("lang=ko_KR")  # 가짜 플러그인 탑재
+    chrome_options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) "
+                                "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36")  # user-agent 이름 설정
+
+    driver = Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    return driver
+
+
+s = CreateSession()
+
+s.get("https://cloud.eais.go.kr/moct/awp/abb01/AWPABB01F01")
+s.implicitly_wait(5)
+
+# ID, PW 입력 후 로그인
+s.find_element(By.ID, 'membId').send_keys('haul1115')
+s.find_element(By.ID, 'pwd').send_keys('ks05090818@')
+s.find_element(By.XPATH, '//*[@id="container"]/div[2]/div/div/div[1]/div[1]/button').click()
+time.sleep(0.5)
+
+s.get('https://cloud.eais.go.kr/moct/bci/aaa02/BCIAAA02L01')
+s.implicitly_wait(5)
+time.sleep(1)
 header = {
-    "Referer": "https://cloud.eais.go.kr/moct/awp/abb01/AWPABB01F01",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.104 Whale/3.13.131.36 Safari/537.36"
+    "Host": "cloud.eais.go.kr",
+    "Content-Type": "application/json;charset=UTF-8",
+    "Referer": "https://cloud.eais.go.kr/moct/bci/aaa02/BCIAAA02L01",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36"
 }
 
-tt = {"inqireGbCd":"0","reqSigunguCd":"11260","bldrgstCurdiGbCd":"0","upperBldrgstSeqno":'100249270',"bldrgstSeqno":""}
+datas = {"query":{"multi_match":{"query":"봉우재로154","type":"cross_fields","operator":"and","fields":["jibunAddr","roadAddr^3"],"tie_breaker":0.3}},"size":20}
+datas2 = {"sort":[{"dongNm":"asc"}],"query":{"bool":{"filter":[{"term":{"mgmUpperBldrgstPk":"11260_10100_0_0090_0029"}}]}},"size":100}
+datas3 = {"addrGbCd":"0","inqireGbCd":"0","bldrgstCurdiGbCd":"0","bldrgstSeqno":"","reqSigunguCd":"11260","sidoClsfCd":"","bjdongCd":"10100","platGbCd":"0","mnnm":"90","slno":"29","splotNm":"","blockNm":"","lotNm":"","roadNmCd":"","bldMnnm":"","bldSlno":"","sigunguCd":"11260"}
+datas4 = {"bldrgstSeqno":2455,"regstrGbCd":"2","regstrKindCd":"2","mjrfmlyIssueYn":"N","locSigunguCd":"11260","locBjdongCd":"10100","locPlatGbCd":"0","locDetlAddr":"서울특별시 중랑구 면목동 90-29 동명칭 없음","locBldNm":"건물명 없음","ownrYn":"N","multiUseBildYn":"N","bldrgstCurdiGbCd":"0"}
 
-# 'http://cloud.eais.go.kr/moct/bci/aaa02/BCIAAA02F01'
+a = s.request('POST', 'https://cloud.eais.go.kr/bldrgstmst/_search', headers=header, json=datas)
+time.sleep(0.5)
+b = s.request('POST', 'https://cloud.eais.go.kr/bldrgsttitle/_search', headers=header, json=datas2)
+time.sleep(0.5)
+c = s.request('POST', 'https://cloud.eais.go.kr/bci/BCIAAA02R01', headers=header, json=datas3)
+time.sleep(0.5)
+d = s.request('POST', 'https://cloud.eais.go.kr/bci/BCIAAA02R01', headers=header, json=datas4)
+time.sleep(0.5)
+# e = s.request('POST', 'https://cloud.eais.go.kr/bci/BCIAAA02R05', headers=header)
+# time.sleep(0.5)
 
-header2 = {"inqireGbCd":"0","reqSigunguCd":"11260","bldrgstCurdiGbCd":"0","upperBldrgstSeqno":'100249270',"bldrgstSeqno":""}
-
-header3 = {"bldrgstSeqno":'100249277',"regstrGbCd":"4","regstrKindCd":"4","mjrfmlyIssueYn":"N","locSigunguCd":"11260","locBjdongCd":"10100","locPlatGbCd":"0","locDetlAddr":"서울특별시 중랑구 면목동 90-27 동명칭 없음 402","locBldNm":"다원빌","ownrYn":"N","multiUseBildYn":"N","bldrgstCurdiGbCd":"0"}
-
-params = {
-    "addrGbCd": "1",
-    "inqireGbCd": "0",
-    "bldrgstCurdiGbCd": "0",
-    "bldrgstSeqno": "",
-    "reqSigunguCd": "11260",
-    "sidoClsfCd": "",
-    "bjdongCd": "",
-    "platGbCd": "", "mnnm": "",
-    "slno": "",
-    "splotNm": "",
-    "blockNm": "",
-    "lotNm": "",
-    "roadNmCd": "112603106002",
-    "bldMnnm": "154",
-    "bldSlno": "0",
-    "sigunguCd": ""
+header2 = {
+    "Host": "cloud.eais.go.kr",
+    "Content-Type": "application/json;charset=UTF-8",
+    "Referer": "https://cloud.eais.go.kr/moct/bci/aaa02/BCIAAA02F01",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36"
 }
 
-# chrome_options = webdriver.ChromeOptions()
-# # chrome_options.add_argument('headless')  # 크롬 화면 숨기기
-# chrome_options.add_argument("no-sandbox")  #
-# chrome_options.add_argument('window-size=1920x1080')  # 해상도 설정
-# chrome_options.add_argument("--start-maximized")
-# chrome_options.add_argument("disable-gpu")  # 가속 사용 x
-# chrome_options.add_argument("lang=ko_KR")  # 가짜 플러그인 탑재
-# chrome_options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) "
-#                             "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36")  # user-agent 이름 설정
-#
-# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-#
-# # 로그인
-# driver.get('http://cloud.eais.go.kr')
-# driver.implicitly_wait(5)
+header3 = {
+    "Host": "cloud.eais.go.kr",
+    "Content-Type": "application/json;charset=UTF-8",
+    "Referer": "https://cloud.eais.go.kr/moct/bci/aaa04/BCIAAA04L01",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36"
+}
 
-# # 알림, 공지 팝업 제거
-# notice_pop_up = driver.find_elements(By.CLASS_NAME, 'swal-button.swal-button--confirm')
-# if len(notice_pop_up): notice_pop_up[0].click()
-# notice_pop_up2 = driver.find_elements(By.CLASS_NAME, 'btnCloseToday')
-# if len(notice_pop_up2): notice_pop_up2[0].click()
-#
-# # '로그인' 버튼 클릭
-# WebDriverWait(driver, 3).until(
-#     ec.presence_of_element_located((By.CLASS_NAME, 'btnLogin.btnLine.btnNormal.btnLine_blue'))).click()
-#
-# # ID, PW 입력 후 로그인인
+datas5 = {"membId":"210706162108haul1115"}
+datas6 = {"lastUpdusrId":"210706162108haul1115"}
+datas7 = {"pbsvcResveDtls":[{"@id":"14b5cc1c-0716-4de2-a65d-fc7c3e73e0f6","rowStatus":"R","firstCrtnDt":"20220319214611","firstWrtrId":"210706162108haul1115","lastUpdtDt":"20220319214611","lastUpdusrId":"210706162108haul1115","untClsfCd":"1008","pbsvcResveDtlsSeqno":"1000000000000049377257","bldrgstSeqno":"2455","regstrGbCd":"2","regstrKindCd":"2","mjrfmlyIssueYn":"N","bldrgstCurdiGbCd":"0","ownrYn":"N","multiUseBildYn":"N","ownrExprsYn":"N","locSigunguCd":"11260","locBjdongCd":"10100","locPlatGbCd":"0","locDetlAddr":"서울특별시 중랑구 면목동 90-29 동명칭 없음","locBldNm":"건물명 없음"}],"ownrExprsYn":"N","pbsvcRecpInfo":{"pbsvcGbCd":"01","issueReadGbCd":"0","pbsvcResveDtlsCnt":1},"appntInfo":{"appntGbCd":"01","appntJmno1":"950509","appntJmno2":"","appntJmno":"","appntBizno":"","appntNm":"장성남","appntMtelno":"","appntSigunguCd":"","naAppntBjdongCd":"","naAppntRoadCd":"","naAppntMnnm":"","naAppntSlno":"","naAppntGrndUgrndGbCd":"0","naAppntDetlAddr":"","appntCorpno":"","appntCoprNm":""}}
+datas8 = {"membNo":"","pbsvcGbCd":"","progStateFlagArr":["01"],"pbsvcProcessGbCd":"","firstSaveStartDate":"2022-02-19","firstSaveEndDate":"2022-03-19","pageNo":0,"recordSize":10}
+# f = s.request('POST', 'https://cloud.eais.go.kr/awp/AWPACC01R03', headers=header2, json=datas5)
+# time.sleep(0.5)
+# g = s.request('POST', 'https://cloud.eais.go.kr/bci/BCIAAA02R05', headers=header2, json=datas6)
+# time.sleep(0.5)
+h = s.request('POST', 'https://cloud.eais.go.kr/bci/BCIAZA02S01', headers=header2, json=datas7)
+time.sleep(0.5)
+# i = s.request('POST', 'https://cloud.eais.go.kr/bci/BCIAAA02D02', headers=header2, json=datas6)
+# time.sleep(0.5)
+j = s.request('POST', 'https://cloud.eais.go.kr/bci/BCIAAA06R01', headers=header3, json=datas8)
+time.sleep(0.5)
+print(a.text)
+print(b.text)
+print(c.text)
+print(d.text)
+# print(e.text)
+# print(f.text)
+# print(g.text)
+# print(h.text)
+# print(i.text)
+print(j.text)
+
+# s.get("https://pro.dabangapp.com/")
+
+
+# # ID, PW 입력 후 로그인
 # driver.find_element(By.ID, 'membId').send_keys(LOGIN_INFO['loginId'])
 # driver.find_element(By.ID, 'pwd').send_keys(LOGIN_INFO['loginPwd'])
 # driver.find_element(By.XPATH, '//*[@id="container"]/div[2]/div/div/div[1]/div[1]/button').click()
@@ -84,29 +123,41 @@ params = {
 #
 # driver.get('http://cloud.eais.go.kr')
 # driver.implicitly_wait(5)
+# custom_header을 통해 아닌 것 처럼 위장하기
+custom_header = {
+    'referer' : 'http://http://finance.daum.net/quotes/A048410#home',
+    'user-agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'  }
 
-with requests.Session() as s:
-    s.verify = False
-
-    s.get('https://cloud.eais.go.kr/')
-
-    login = {'loginId': 'ks050940', 'loginPwd': 'ks05090818@'}
-    login_req = s.post('https://cloud.eais.go.kr/moct/awp/abb01/AWPABB01F01/', headers=header, json=login)
-    print(login_req.json()['accessToken'])
-    for c in login_req.cookies:
-        print(f'aa: {c}')
-        driver.add_cookie({'name': c.name, 'value': c.value, 'path': c.path, 'expiry': c.expires})
-
-    headers = {
-        "Referer": "https://cloud.eais.go.kr/moct/bci/aaa02/BCIAAA02L01",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.104 Whale/3.13.131.36 Safari/537.36"
-    }
-
-    aa = s.post('https://cloud.eais.go.kr/moct/bci/aaa02/BCIAAA02L01', headers=headers, data=params)
-
-    params2 = {"bldrgstSeqno":'100249270',"regstrGbCd":"3","regstrKindCd":"3","mjrfmlyIssueYn":"N","locSigunguCd":"11260","locBjdongCd":"10100","locPlatGbCd":"0","locDetlAddr":"서울특별시 중랑구 면목동 90-27 동명칭 없음","locBldNm":"다원빌","ownrYn":"N","multiUseBildYn":"N","bldrgstCurdiGbCd":"0"}
-
-    bbs = s.post('https://cloud.eais.go.kr/moct/bci/aaa02/BCIAAA02L01', headers=headers, data=params2)
+#해당 접속 사이트가 아닌 원본데이터가 오는 url 추적. network에서 가지고 온다.
+# url = "http://finance.daum.net/api/search/ranks?limit=10"
+#
+# req = requests.get(url, headers = custom_header)    #custom_header를 사용하지 않으면 접근 불가
+#
+# if req.status_code == requests.codes.ok:
+#     print("접속 성공")
+#     stock_data = json.loads(req.text)        #json에 반환된 데이터가 들어가 있다.
+#     print(req.text)
+#     for rank in stock_data['data']:         #stock_data는 'data' key값에 모든 정보가 들어가 있다.
+#         print(rank['rank'], rank['symbolCode'], rank['name'], rank['tradePrice'])
+#
+# with requests.Session() as s:
+#     s.verify = False
+#
+#     aas = s.get('https://cloud.eais.go.kr/', headers=header)
+#
+#     login = {'loginId': 'ks050940', 'loginPwd': 'ks05090818@'}
+#     login_req = s.post('https://cloud.eais.go.kr/moct/awp/abb01/AWPABB01F01/', headers=header, json=login)
+#
+#     headers = {
+#         "Referer": "https://cloud.eais.go.kr/moct/bci/aaa02/BCIAAA02L01",
+#         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.104 Whale/3.13.131.36 Safari/537.36"
+#     }
+#
+#     aa = s.post('https://cloud.eais.go.kr/moct/bci/aaa02/BCIAAA02L01', headers=headers, data=params)
+#
+#     params2 = {"bldrgstSeqno":'100249270',"regstrGbCd":"3","regstrKindCd":"3","mjrfmlyIssueYn":"N","locSigunguCd":"11260","locBjdongCd":"10100","locPlatGbCd":"0","locDetlAddr":"서울특별시 중랑구 면목동 90-27 동명칭 없음","locBldNm":"다원빌","ownrYn":"N","multiUseBildYn":"N","bldrgstCurdiGbCd":"0"}
+#
+#     bbs = s.post('https://cloud.eais.go.kr/moct/bci/aaa02/BCIAAA02L01', headers=headers, data=params2)
     #print(bbs.content)
 
     # # 웹 드라이버 쿠키
